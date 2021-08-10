d293255013c8abebe071f65f1fe9df72ea58afeb
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-08-10 08:40:31 -0400

    chore: copy over code from repo_commits

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,58 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import { Octokit } from "@octokit/rest";
+import * as core from "@actions/core";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const octokit = new Octokit({
+      auth: core.getInput("token"),
+    });
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const owner = core.getInput("owner");
+    const repo = core.getInput("repo");
+    const basehead = core.getInput("basehead");
 
-    core.setOutput('time', new Date().toTimeString())
+    const header = ["SHA256", "Commit Message", "Timestamp"];
+    const headerLine = `|${header.join("|")}|`;
+    const headerSepLine = `|${new Array(header.length).fill("---").join("|")}|`;
+    const headerLines = [headerLine, headerSepLine];
+    const shaLength: number = JSON.parse(core.getInput("sha-length"));
+    const showMergeCommits: boolean = JSON.parse(
+      core.getInput("show-merge-commits")
+    );
+
+    const lines = [];
+
+    for await (const {
+      data: { commits },
+    } of octokit.paginate.iterator(
+      octokit.rest.repos.compareCommitsWithBasehead, // eslint-disable-line indent
+      { owner, repo, basehead } // eslint-disable-line indent
+    )) /* eslint-disable-line indent */ {
+      for (const {
+        sha,
+        html_url: shaUrl,
+        commit: { message, committer },
+      } of commits.filter(c => showMergeCommits || c.parents.length < 2)) {
+        const sha256 = sha.slice(0, shaLength);
+        const commitMessage = message.split("\n")[0];
+        const date = committer?.date ?? "unknown";
+
+        const fields = [
+          `[\`${sha256}\`](${shaUrl})`,
+          `\`${commitMessage}\``,
+          `\`${date.replace("T", " ")}\``,
+        ];
+        lines.push(`|${fields.join("|")}|`);
+      }
+    }
+
+    core.setOutput(
+      "differences",
+      headerLines.concat(lines.reverse()).join("\n")
+    );
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
