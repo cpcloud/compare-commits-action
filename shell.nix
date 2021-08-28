07f4f66b59294a72e1a9a45d9cae33b986568ab6
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-08-28 09:28:09 -0400

    chore: add pre-commit setup

--- shell.nix
+++ shell.nix
@@ -5,13 +5,19 @@ in
 pkgs.mkShell {
   name = "compare-commits-action";
   buildInputs = with pkgs; [
+    git
     niv
+    nix-linter
+    nixpkgs-fmt
     nodejs_latest
+    shellcheck
+    shfmt
   ];
 
   # npm forces output that can't possibly be useful to stdout so redirect
   # stdout to stderr
   shellHook = ''
+    ${(import ./pre-commit.nix).pre-commit-check.shellHook}
     npm install --no-fund 1>&2
   '';
 }
